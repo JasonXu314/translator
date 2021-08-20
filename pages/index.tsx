7fb2241e8378dec452e4ba17bfd1f4cbf27a788e
JasonDing9 <pengjding@gmail.com>
2021-08-20 10:01:33 -0700

    Added Title and MetaData

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { kMaxLength } from 'buffer';
 import { NextPage } from 'next/types';
 import { useEffect, useMemo, useState } from 'react';
 import Button from '../components/Button/Button';
