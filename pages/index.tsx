1afb7bc141e8d1269941cf3df88cda13f37d4fec
Jason Xu <Xinwen31415@gmail.com>
2021-11-08 21:48:06 -0600

    changed to new url

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,18 +12,13 @@ const Index: NextPage = () => {
 	const [output, setOutput] = useState<string>('');
 
 	useEffect(() => {
-		axios.post('https://interlinguatranslatorengtoina.herokuapp.com/wakeup').catch(() => {});
-		axios.post('https://interlinguatranslatorinatoeng.herokuapp.com/wakeup').catch(() => {});
-	}, []);
-
-	useEffect(() => {
 		if (input.trim().length > 0) {
 			const { token, cancel } = axios.CancelToken.source();
 
 			const timeout = setTimeout(() => {
 				setOutput('Loading Translation...');
 				axios
-					.get(`https://interlinguatranslator${inputLang}to${outputLang}.herokuapp.com/?text=${input}`, {
+					.get(`http://18.26.2.114:5000/?text=${input}&direction=${outputLang === 'ina' ? 1 : 0}`, {
 						cancelToken: token
 					})
 					.then((res) => {
@@ -55,22 +50,16 @@ const Index: NextPage = () => {
 			</Head>
 			<div className={styles.welcome}>
 				<h1>Online Interlingua-English Translator</h1>
-				Welcome to the first online, public, and free and open source Interlingua and English translator! Input
-				your text in the left text box and the translated sentence will be shown in the right text box. Click the
-				&quot;Change Language&quot; button below to change the translator&apos;s direction.
+				Welcome to the first online, public, and free and open source Interlingua and English translator! Input your text in the left text box and the
+				translated sentence will be shown in the right text box. Click the &quot;Change Language&quot; button below to change the translator&apos;s
+				direction.
 			</div>
 			<div className={styles['lang-row']}>
 				<h4 className={styles.lang}>{inputLang === 'eng' ? 'English' : 'Interlingua'}</h4>
 				<h4 className={styles.lang}>{outputLang === 'eng' ? 'English' : 'Interlingua'}</h4>
 			</div>
 			<div className={styles['control-row']}>
-				<textarea
-					rows={12}
-					cols={96}
-					value={input}
-					maxLength={999}
-					onChange={(evt) => setInput(evt.target.value)}
-				/>
+				<textarea rows={12} cols={96} value={input} maxLength={999} onChange={(evt) => setInput(evt.target.value)} />
 				<div className={styles['btn-container']}>
 					<Button
 						onClick={() => {
@@ -86,27 +75,23 @@ const Index: NextPage = () => {
 			<br></br>
 			<div className={styles.information}>
 				<h2>Information</h2>
-				Interlingua is an international auxiliary language (IAL). IALs, such as Esperanto, are artificial
-				languages that are constructed to simplify communication between speakers who do not share a common first
-				language. Interlingua’s vocabulary and grammar are derived from a combination of different Romance
-				languages. Therefore, many schools and universities teach Interlingua as a way of teaching other Romance
-				languages more easily, and Interlingua has been embraced by parts of the scientific community. <br></br>{' '}
-				<br></br>
-				Despite its increasing popularity, Interlingua is not supported by online translation tools such as Google
-				Translate. The purpose of this research project is to bridge this gap, by creating the first computer
-				program that can translate text between English and Interlingua. The hope was that the translation tool
-				would act as a valuable educational resource to Interlingua learners, and it might spread awareness of
-				Interlingua to a broader audience. <br></br>
+				Interlingua is an international auxiliary language (IAL). IALs, such as Esperanto, are artificial languages that are constructed to simplify
+				communication between speakers who do not share a common first language. Interlingua’s vocabulary and grammar are derived from a combination of
+				different Romance languages. Therefore, many schools and universities teach Interlingua as a way of teaching other Romance languages more easily,
+				and Interlingua has been embraced by parts of the scientific community. <br></br> <br></br>
+				Despite its increasing popularity, Interlingua is not supported by online translation tools such as Google Translate. The purpose of this research
+				project is to bridge this gap, by creating the first computer program that can translate text between English and Interlingua. The hope was that
+				the translation tool would act as a valuable educational resource to Interlingua learners, and it might spread awareness of Interlingua to a
+				broader audience. <br></br>
 			</div>
 			<br></br>
 			<div className={styles.information}>
 				<h2>More Resources</h2>
 				<div className={styles['more-resources']}>
 					<h4>Interlingua Corpus Project</h4>
-					Created by Jason Ding with the help of his mentor, Todd Mockler, the Interlingua Corpus Project has a
-					collection of over 1.2 million quality-controlled Interlingua sentences scraped from an
-					individually-designed web crawler that has visited over 6 million relevant websites, while also
-					providing over 80,000 parallel Interlingua-English sentences and other useful data.
+					Created by Jason Ding with the help of his mentor, Todd Mockler, the Interlingua Corpus Project has a collection of over 1.2 million
+					quality-controlled Interlingua sentences scraped from an individually-designed web crawler that has visited over 6 million relevant websites,
+					while also providing over 80,000 parallel Interlingua-English sentences and other useful data.
 					<br></br>
 					<a target="_blank" rel="noreferrer noopener" href="http://www.interlinguacorpus.org/">
 						Link to the Interlingua Corpus Project
@@ -114,23 +99,17 @@ const Index: NextPage = () => {
 					<br></br>
 					<br></br>
 					<h4>Google Colab Interlingua Translator</h4>
-					Created by Jason Ding with the help of his mentor, Todd Mockler, the first online, public, and free
-					Interlingua and English translator was created. This translator uses the strongest Interlingua-English
-					translator model there is to translate in both directions between English and Interlinuga.
+					Created by Jason Ding with the help of his mentor, Todd Mockler, the first online, public, and free Interlingua and English translator was
+					created. This translator uses the strongest Interlingua-English translator model there is to translate in both directions between English and
+					Interlinuga.
 					<br></br>
-					The model has scored a BLEU score of 42.45 going from English to Interlingua and a 42.54 going from
-					Interlingua to English, both of which are &quot;high quality translation&quot; according to one of{' '}
-					<a
-						target="_blank"
-						rel="noreferrer noopener"
-						href="https://cloud.google.com/translate/automl/docs/evaluate">
+					The model has scored a BLEU score of 42.45 going from English to Interlingua and a 42.54 going from Interlingua to English, both of which are
+					&quot;high quality translation&quot; according to one of{' '}
+					<a target="_blank" rel="noreferrer noopener" href="https://cloud.google.com/translate/automl/docs/evaluate">
 						Google&apos;s articles.
 					</a>
 					<br></br>
-					<a
-						target="_blank"
-						rel="noreferrer noopener"
-						href="https://colab.research.google.com/drive/1SFewmDos1Z-Gq9z6OB9tgxaBsNdZvaT3?usp=sharing">
+					<a target="_blank" rel="noreferrer noopener" href="https://colab.research.google.com/drive/1SFewmDos1Z-Gq9z6OB9tgxaBsNdZvaT3?usp=sharing">
 						Link to the Google Collab Interlingua-English Translator
 					</a>
 					<br></br>
@@ -170,19 +149,11 @@ const Index: NextPage = () => {
 			<div className={styles['viewer-counts']}>
 				<div className={styles.regular}>
 					This site has been visited{' '}
-					<img
-						src="https://hitwebcounter.com/counter/counter.php?page=7856187&style=0008&nbdigits=3&type=page&initCount=0"
-						alt="web counter"
-					/>{' '}
-					times
+					<img src="https://hitwebcounter.com/counter/counter.php?page=7856187&style=0008&nbdigits=3&type=page&initCount=0" alt="web counter" /> times
 				</div>
 				<div className={styles.unique}>
 					This site has been visited by{' '}
-					<img
-						src="https://hitwebcounter.com/counter/counter.php?page=7856188&style=0008&nbdigits=3&type=ip&initCount=0"
-						alt="web counter"
-					/>{' '}
-					people
+					<img src="https://hitwebcounter.com/counter/counter.php?page=7856188&style=0008&nbdigits=3&type=ip&initCount=0" alt="web counter" /> people
 				</div>
 				<div>
 					This website&apos;s source can be found{' '}
