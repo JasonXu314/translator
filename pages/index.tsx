5daea23b922b1ba387de698cf7c6af81d7c9faa0
JasonDing9 <pengjding@gmail.com>
2021-11-20 15:50:10 -0800

    Added Stuffs

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,6 +38,12 @@ const Index: NextPage = () => {
 
 	return (
 		<div className={styles.main}>
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"></link>
+
+			<script  src="http://code.jquery.com/jquery-3.3.1.js"   integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="></script>
+
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"></script>
+			
 			<Head>
 				<title>Online Interlingua-English Translator</title>
 				<meta name="description" content="Interlingua translator provides Interlingua vocabulary and grammar in context for language learners and linguistic analyses." />
